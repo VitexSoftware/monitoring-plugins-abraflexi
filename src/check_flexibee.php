063b76d3d1535fab657baa5d61a412c39db337f5
Vítězslav Dvořák <info@vitexsoftware.cz>
2018-08-08 10:54:41 +0200

    License expiration check added

--- src/check_flexibee.php
+++ src/check_flexibee.php
@@ -1,7 +1,7 @@
 #!/usr/bin/env php
 <?php
 /**
- * monitoring plugins for FlexiBee - Basic Check
+ * monitoring plugins for FlexiBee - Basic Check - accessibility & license
  *
  * @author     Vítězslav Dvořák <info@vitexsoftware.cz>
  * @copyright  2017-2018 Vitex Software
@@ -15,10 +15,24 @@ $infodata = $checker->getDataValue('status');
 
 if (is_null($infodata)) {
     $result  = 'CRITICAL';
-    $message = $checker->lastResponseCode.' '.$checker->lastCurlError.' ('.$checker->apiURL.')';
+    $message = $checker->lastResponseCode.' '.$checker->lastCurlError;
 } else {
-    $result  = 'OK';
-    $message = 'FlexiBee '.$infodata['version'].' ('.$checker->url.')';
+    $licensedataRaw = $checker->getFlexiData('/default-license');
+    $licensedata    = $licensedataRaw['license'];
+    $licenseExpire  = new DateTime($licensedata['validTo']);
+    $diff           = intval($licenseExpire->diff(new DateTime())->format("%a"));
+    if ($diff < 7) {
+        if ($diff < 0) {
+            $result  = 'ERROR';
+            $message = 'FlexiBee '.$infodata['version'].' License Expired';
+        } else {
+            $result  = 'WARNING';
+            $message = 'FlexiBee '.$infodata['version'].' License Expire in '.$diff.' days';
+        }
+    } else {
+        $result  = 'OK';
+        $message = 'FlexiBee '.$infodata['version'];
+    }
 }
 
-exit(\MPF\Connector::returnExitCode($result, $message));
+exit(\MPF\Connector::returnExitCode($result, $message.' ('.$checker->url.')'));
